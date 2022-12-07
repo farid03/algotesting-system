3ecbd9a797a5da84f9b74239403c69d9a1aa7296
Farid Kurbanov <F.Kurbanov120303@yandex.ru>
2022-12-08 02:20:36 +0300

    changed program logging to console

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"dataprocessing"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -17,19 +18,18 @@ func main() {
 
 	testsSrcFile, err := os.Open(args[3])
 	if err != nil {
-		fmt.Println("Unable to open tests source file:", err)
+		log.Println("Unable to open tests source file:", err)
 		os.Exit(1)
 	}
 	testCases := dataprocessing.SplitTests(testsSrcFile)
-	fmt.Println("The tests are ready to run. In total", len(testCases), "cases.")
+	log.Println("The tests are ready to run. In total", len(testCases), "cases.")
 
 	var firstProgramName = args[1]
 	var secondProgramName = args[2]
 	dataprocessing.StartTests(firstProgramName, secondProgramName, testCases, "result.txt")
-
-	fmt.Println("The output is in \"result.txt\".")
+	//TODO добавить возможность задавать файл для выходных данных
+	log.Println("The output is in \"result.txt\".")
 }
 
 // TODO добавить режим быстрой прогонки тестов без записи правильных ответов в файл, только неправильные || DEFAULT
 // TODO режим без записи любых результатов, только результат совпадения во всех тестах
-// TODO добавить сборки под linux + win x86_x64 && arm
